825126fb97462c3c9eb318f29eb2468a7958444d
Yuki Ishibashi <ishibash@adobe.com>
2016-09-13 12:42:47 -0400

    Add Sumo line: APP_NAME=chronos-cleanup to any chronos-cleanup.sh output

--- v1/util/chronos-cleanup.sh
+++ v1/util/chronos-cleanup.sh
@@ -4,6 +4,7 @@ CHRONOS_URL="http://localhost"
 CHRONOS_PORT="4400"
 CHRONOS_USERNAME="$(etcdctl get /chronos/config/username)"
 CHRONOS_PASSWORD="$(etcdctl get /chronos/config/password)"
+SUMO_FILTER="APP_NAME='chronos-cleanup'"
 if [[ "$CHRONOS_USERNAME" != "" && "$CHRONOS_PASSWORD" != "" ]]; then
   $CHRONOS_AUTH="-u ${CHRONOS_USERNAME}:${CHRONOS_PASSWORD}"
 else
@@ -13,11 +14,12 @@ ALL_JOBS="$(curl ${CHRONOS_AUTH} -L -X GET ${CHRONOS_URL}:${CHRONOS_PORT}/schedu
 OLD_JOBS="$(echo "$ALL_JOBS" | grep '^R0/')"
 OLD_JOB_NAMES="$( echo "$OLD_JOBS" | awk -F ',' '{print $2}' )"
 if [[ "$OLD_JOB_NAMES" != "" ]] ; then
-  echo -e "Old jobs found:\n$OLD_JOB_NAMES\n"
+  echo -e "$SUMO_FILTER Old jobs found:\n$OLD_JOB_NAMES\n"
   for job_name in $OLD_JOB_NAMES ; do 
-    echo "Deleting $job_name..."
-    curl --write-out '  curl return code: %{http_code}\n' -L -X DELETE ${CHRONOS_URL}:${CHRONOS_PORT}/scheduler/job/${job_name}
+    echo -e "$SUMO_FILTER Deleting $job_name \n"
+    DELETE_RETURN_CODE="$(curl --write-out '  curl return code: %{http_code}\n' -L -X DELETE ${CHRONOS_URL}:${CHRONOS_PORT}/scheduler/job/${job_name} )"
+    echo -e "$SUMO_FILTER delete curl code for job $job_name: $DELETE_RETURN_CODE"
   done
 else 
-  echo "No old jobs to be deleted!"
+  echo "$SUMO_FILTER No old jobs found to be deleted."
 fi
